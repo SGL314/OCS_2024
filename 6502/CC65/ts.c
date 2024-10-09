1735fb54af5bb638f9d4983e0772f719b8adcd5c
SGL314 <samugonlob12@gmail.com>
2024-10-08 23:42:21 -0300

    vascool

---
+++ 6502/CC65/ts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    // Função padrão para imprimir uma string
+    printf("Hello, 6502 World!\n");
+
+    // Exemplo de loop simples
+    for (int i = 0; i < 5; i++) {
+        printf("Loop iteration: %d\n", i);
+    }
+
+    // Finaliza o programa
+    printf("Program has ended.\n");
+}
